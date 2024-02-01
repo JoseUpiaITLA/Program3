c80c7477fdae7a54b4c9cdbfdf54144e8efc167e
Jose Miguel Upia <20198757@itla.edu.do>
2024-02-01 08:21:41 -0400

    Add project files.

---
+++ Program3/Given.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Program3
+{
+    public class Given
+    {
+        public int FacesGiven = 0;
+        public Given()
+        {
+            Random rand = new Random();
+            FacesGiven = rand.Next(1, 7);
+        }
+
+    }
+}
---
+++ Program3/Program.cs
@@ -0,0 +1,39 @@
+﻿using Program3;
+
+int menu = 0;
+while (true)
+{
+    try
+    {
+        Console.WriteLine($"\n1. Lanzar dados \n2. Salir del juego");
+        menu = int.Parse(Console.ReadLine());
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Ingrese un numero valido");
+        continue;
+    }
+
+    if (menu == 2)
+        break;
+
+    if (menu != 1)
+        continue;
+
+    Given given1 = new Given();
+    Given given2 = new Given();
+    Given given3 = new Given();
+
+    Console.WriteLine($"\nNumeros de los dados: {given1.FacesGiven} - {given2.FacesGiven} - {given3.FacesGiven}");
+
+    if (given1.FacesGiven == given2.FacesGiven && given2.FacesGiven == given3.FacesGiven)
+    {
+        Console.WriteLine("Gano");
+        continue;
+    } 
+    else
+    {
+        Console.WriteLine("Perdio");
+        continue;
+    }
+}
